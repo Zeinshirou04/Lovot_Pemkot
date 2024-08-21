9a63bf5e4e9b7adb94c2359a64b47fe607cb571a
Zen <Farras.FF6@gmail.com>
2024-08-22 01:11:44 +0700

    Commit Update
    - Released Capture Voice

--- app.py
+++ app.py
@@ -4,13 +4,16 @@ import vertexai
 from vertexai.generative_models import GenerativeModel, Part
 import vertexai.preview.generative_models as generative_models
 from dotenv import load_dotenv
+import speech_recognition as sr
 
 class Lovot:
     message = ""
     model = ""
     chat = ""
+    recognizer = ""
 
-    text1_1 = """Halo, disini aku akan memberikan mu sebuah identitas untuk deployment mu.
+    text1_1 = """
+    Halo, disini aku akan memberikan mu sebuah identitas untuk deployment mu.
 
     Namamu: Lovot
     Pembuat: Pemkot Semarang dan Fakultas Teknik UDINUS (Universitas Dian Nuswantoro)
@@ -20,7 +23,8 @@ class Lovot:
 
     Beberapa aturan yang perlu kamu atuhi
     1. Dilarang menggunakan markdown, dan juga emoji
-    2. Dilarang menjawab pertanyaan tidak jelas atau noise dan cukup diam jika termasuk dalam kategori tersebut seperti hanya mengembalikan response berupa string kosong"""
+    2. Dilarang menjawab pertanyaan tidak jelas atau noise dan cukup diam jika termasuk dalam kategori tersebut seperti hanya mengembalikan response berupa string kosong
+    """
 
     generation_config = {
         "max_output_tokens": 8192,
@@ -36,14 +40,20 @@ class Lovot:
     }
     
     def __init__(self):
+        self.recognizer  = sr.Recognizer()
         load_dotenv()
-        # print(os.getenv('PROJECT_NAME'))
-        vertexai.init(project=os.getenv('PROJECT_NAME'), location=os.getenv('PROJECT_LOCATION'))
-        self.model = GenerativeModel(
-            "gemini-1.5-flash-001",
-        )
-        self.chat = self.model.start_chat()
-        self.multiturn_generate_content()
+
+        '''
+        Silahkan un-comment code dibawah saat deployment
+        Jika terkena limit quota, maka silahkan di comment
+        '''
+
+        # vertexai.init(project=os.getenv('PROJECT_NAME'), location=os.getenv('PROJECT_LOCATION'))
+        # self.model = GenerativeModel(
+        #     "gemini-1.5-flash-001",
+        # )
+        # self.chat = self.model.start_chat()
+        # self.multiturn_generate_content()
 
     def multiturn_generate_content(self):
         self.chat.send_message(
@@ -62,6 +72,30 @@ class Lovot:
             safety_settings=self.safety_settings
         ).text)
         
+    def capture_voice(self):
+        with sr.Microphone() as source:
+            self.recognizer.adjust_for_ambient_noise(source=source, duration=0.1)
+            print("Mendengarkan...")
+            audio = self.recognizer.listen(source=source)
+            text = self.convert_stt(audio=audio)
+        return text    
+    
+    def convert_stt(self, audio):
+        text = ""
+        try:
+            
+            '''
+            Jika terjadi error pada line berikute, silahkan meminta Credentials JSON pada Farras Adhani Zayn
+            '''
+            
+            text = self.recognizer.recognize_google_cloud(audio_data=audio, credentials_json=os.getenv('CREDENTIAL_JSON_PATH'), language="id-ID")
+            print(f"Pesan = {text}")
+        except sr.UnknownValueError:
+            print("Kesalahan Nilai")
+        except sr.RequestError as e:
+            print(f"Error: {e}")
+        return text
+    
     def message(self, text = ""):
         if text == "": text = input("Masukkan Pesan: ")
         print(self.response(text))
@@ -78,6 +112,7 @@ class Lovot:
 try:
     while True:
         lovot = Lovot()
-        lovot.run()
+        lovot.capture_voice()
+        # lovot.run()
 except KeyboardInterrupt:
     print("\nPrgoram Selesai")
--- readme.md
+++ readme.md
@@ -1,12 +1,17 @@
 # Lovot Pemkot Documentation
 
-# Mohon diperhatikan program dan library yang digunakan
+### Mohon diperhatikan program dan library yang digunakan
 
 Library yang digunakan sudah tertera pada requirements.txt beberapanya termasuk Gemini
+
+Sebelum menggunakan, jika ingin menggunakan virtual environment pada lovot silahkan copy paste command berikut di CMD dengan path `/Lovot_Pemkot`
+
+`/lovot-venv/Scripts/activate`
+
 Cukup jalankan `pip install -r requirements.txt` untuk menginstall library yang diperlukan.
 Dengan syarat path saat ini di CMD / Terminal berada pada `/Lovot_Pemkot/`
 
 Sebelum melakukan runnning. Silahkan untuk menginstall gcloud cli terlebih dahulu pada link berikut 
-[Google Cloud](https://cloud.google.com/sdk/docs/instal "Instalasi Google Cloud CLI") dan ikuti langkah-langkah yang tertera
+[Google Cloud Installation Documentation](https://cloud.google.com/sdk/docs/instal "Instalasi Google Cloud CLI") dan ikuti langkah-langkah yang tertera
 
-Untuk Informasi lebih lanjut dan Akun authentikasi silahkan hubungi: Farras
+Untuk Informasi lebih lanjut dan Akun authentikasi silahkan hubungi: [Farras Adhani Zayn](farras.ff6@gmail.com)
