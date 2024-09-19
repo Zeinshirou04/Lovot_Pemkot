a283aa9f8addae469dac6fc6fae747eb7022a47e
Zen <Farras.FF6@gmail.com>
2024-09-19 19:18:16 +0700

    Commit Changes

--- App/Flask/__init__.py
+++ App/Flask/__init__.py
@@ -4,5 +4,5 @@ from Flask.blueprints.display import EyesBP
 app = Flask(__name__)
 app.register_blueprint(EyesBP)
 
-if __name__ == '__main__':
-    app.run(debug=True)
+def create_app():
+    return app
